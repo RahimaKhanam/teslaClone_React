c199d2f2aff8cc8c040ac18e100e97eb5809a20b
RahimaKhanam <rahima.khanam45@gmail.com>
2021-11-23 19:29:00 +0530

    BurgerNav section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
+import { useSelector } from 'react-redux';
+import { selectCars } from '../features/car/carSlice';
 
 function Header() {
+    const [burgerStatus, setburgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
+
     return (
         <Container>
             <a>
                 <img src="/images/logo.svg" alt="logo-_image" />
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                {cars && cars.map((car, index) => (
+                    <a key={index} href="#">{car}</a>
+                ))}
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
                 <a href="#">Tesla Account</a>
-                <CustomMenu />
+                <CustomMenu onClick={() => setburgerStatus(true)} />
             </RightMenu>
+            <BurgerNav show={burgerStatus}>
+                <CloseWrapper>
+                    <CustomClose onClick={() => setburgerStatus(false)} />
+                </CloseWrapper>
+                {cars && cars.map((car, index) => (
+                    <li key={index}><a href="">{car}</a></li>
+                ))}
+
+                <li><a href="">Existing Inventory</a></li>
+                <li><a href="">Used Inventory</a></li>
+                <li><a href="">Trade-in</a></li>
+                <li><a href="">Cybertruck</a></li>
+                <li><a href="">Roadaster</a></li>
+                <li><a href="">Semi</a></li>
+                <li><a href="">Commercial Energy</a></li>
+                <li><a href="">Test Drive</a></li>
+            </BurgerNav>
         </Container>
     )
 }
@@ -35,6 +57,7 @@ display: flex;
 align-items: center;
 justify-content: space-between;
 padding: 0 20px;
+z-index: 1;
 `;
 
 const Menu = styled.div`
@@ -67,6 +90,41 @@ a {
 `;
 
 const CustomMenu = styled(MenuIcon)`
+cursor: pointer;
+
+`;
+
+const BurgerNav = styled.div`
+position: fixed;
+top: 0;
+bottom: 0;
+right: 0;
+background: white;
+width: 300px;
+z-index: 16;
+list-style: none;
+padding: 20px;
+display: flex;
+flex-direction: column;
+text-align: start;
+transform: ${props => props.show ? 'translate(0)' : 'translate(100%)'};
+transition: transform 0.2s;
+li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+        font-weight: 600;
+    }
+}
+`;
+
+const CloseWrapper = styled.div`
+display: flex;
+justify-content: flex-end;
+`;
+
+const CustomClose = styled(CloseIcon)`
 cursor: pointer;
 
 `;
